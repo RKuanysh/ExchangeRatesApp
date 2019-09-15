3f0f0ff53bba82756b56c07bc8db3e838b18dac5
Kuanysh Raimbekov <github.com@k.otherinbox.com>
2019-09-15 21:46:35 +0600

    FIXED only positive decimal inputs allowed into value field

--- app/src/main/java/com/raimbekov/rates/main/view/model/RatesUpdateData.kt
+++ app/src/main/java/com/raimbekov/rates/main/view/model/RatesUpdateData.kt
@@ -1,7 +1,5 @@
 package com.raimbekov.rates.main.view.model
 
-import com.raimbekov.rates.main.domain.model.Rate
-
 data class RatesUpdateData(
     val rates: List<RateViewData>,
     val isFirstChanged: Boolean = false
