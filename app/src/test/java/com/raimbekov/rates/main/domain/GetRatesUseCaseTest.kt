7124529b2f6edaeefee5f529e3bd2816b371dd87
Kuanysh Raimbekov <github.com@k.otherinbox.com>
2019-09-17 09:45:37 +0600

    ADDED GetRatesUseCase unit tests

--- app/src/test/java/com/raimbekov/rates/main/domain/GetRatesUseCaseTest.kt
+++ app/src/test/java/com/raimbekov/rates/main/domain/GetRatesUseCaseTest.kt
@@ -28,55 +28,67 @@ class GetRatesUseCaseTest {
 
     @Test
     fun `getRates - empty rates list`() {
-        val currency = "EUR"
         every { ratesRepository.getRates(any()) } returns Flowable.just(emptyList())
 
-        val subscription = getRatesUseCase.getRates(currency, 1.0).test()
+        val subscription = getRatesUseCase.getRates("EUR", 1.0).test()
 
         subscription.assertValueCount(1)
             .assertValueAt(0, emptyList())
+            .dispose()
     }
 
     @Test
     fun `getRates - non empty rates list`() {
-        val currency = "EUR"
         val rates = stubRatesList()
 
         every { ratesRepository.getRates(any()) } returns Flowable.just(rates)
 
-        val subscription = getRatesUseCase.getRates(currency, 1.0).test()
+        val subscription = getRatesUseCase.getRates("EUR", 1.0).test()
 
         subscription.assertValueCount(1)
             .assertValueAt(0, rates)
+            .dispose()
     }
 
     @Test
     fun `getRates - amount multiplication`() {
-        val currency = "EUR"
         val amount = 2.0
         val rates = stubRatesList()
         val expectedRates = rates.map { it.copy(value = it.value * amount) }
 
         every { ratesRepository.getRates(any()) } returns Flowable.just(rates)
 
-        val subscription = getRatesUseCase.getRates(currency, amount).test()
+        val subscription = getRatesUseCase.getRates("EUR", amount).test()
 
         subscription.assertValueCount(1)
             .assertValueAt(0, expectedRates)
+            .dispose()
     }
 
     @Test
     fun `getRates - flow of rates`() {
-        val currency = "EUR"
         val rates = stubRatesList()
 
         every { ratesRepository.getRates(any()) } returns Flowable.just(rates).startWith(emptyList<Rate>())
 
-        val subscription = getRatesUseCase.getRates(currency, 1.0).test()
+        val subscription = getRatesUseCase.getRates("EUR", 1.0).test()
 
         subscription.assertValueCount(2)
             .assertValueAt(0, emptyList())
             .assertValueAt(1, rates)
+            .dispose()
+    }
+
+    @Test
+    fun `getRates - error`() {
+        val error = Exception()
+        every { ratesRepository.getRates(any()) } returns Flowable.error(error)
+
+        val subscription = getRatesUseCase.getRates("EUR", 1.0).test()
+
+        subscription
+            .assertError(error)
+            .dispose()
     }
 
     private fun stubRatesList(): List<Rate> {
